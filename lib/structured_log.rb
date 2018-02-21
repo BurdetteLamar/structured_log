c11923ebee37ee7c4cfcfc85ed94bc54e85e95cf
burdettelamar <BurdetteLamar@Yahoo.com>
2018-02-21 17:13:56 -0600

    More on log

---
+++ lib/structured_log.rb
@@ -0,0 +1,273 @@
+require 'rexml/document'
+require 'nokogiri'
+
+class StructuredLog
+
+  attr_accessor \
+    :file,
+    :file_path,
+    :backtrace_filter,
+    :root_name,
+    :xml_indentation
+
+  include REXML
+
+  DEFAULT_FILE_NAME = 'log.xml'
+  DEFAULT_DIR_PATH = '.'
+  DEFAULT_XML_ROOT_TAG_NAME = 'log'
+  DEFAULT_XML_INDENTATION = -1
+
+  # Message for no block error.
+  NO_BLOCK_GIVEN_MSG = 'No block given'
+  # Message for calling-new error.
+  NO_NEW_MSG = format('Please use %s.open, not %s.new.', self.class.name, self.class.name)
+
+  # Callers should call this method, not method +new+.
+  # Options can include:
+  # - :file_path => _path_.
+  # - :root_name => _root-xml-tag-name_.
+  # - :xml_indentation => Integer:  indentation for nesting XML sub-elements.
+  def self.open(options=Hash.new)
+    raise NO_BLOCK_GIVEN_MSG unless (block_given?)
+    default_options = Hash[
+        :file_path => File.join(DEFAULT_DIR_PATH, DEFAULT_FILE_NAME),
+        :root_name => DEFAULT_XML_ROOT_TAG_NAME,
+        :xml_indentation => DEFAULT_XML_INDENTATION
+    ]
+    options = default_options.merge(options)
+    log = self.new(options, im_ok_youre_not_ok = true)
+    yield log
+    log.send(:dispose)
+    log.file_path
+  end
+
+  # Log an XML element.
+  # - +element_name+:  Element name for logged element.
+  # - *+args+:  Anything;  processed left to right;  for each _arg_:
+  #   - +Hash+:  becomes element attributes.
+  #   - +String+:  appended to PCDATA.
+  #   - +:timestamp+:  causes a timestamp to be added to the element.
+  #   - +:duration+:  causes block's duration to be added to the element.
+  #   - +:rescue+:  causes any exception in block to be rescued and logged.
+  #   - else:  _arg_.inspect is appended to PCDATA.
+  def put_element(element_name = 'element', *args)
+    attributes = {}
+    pcdata = ''
+    start_time = nil
+    duration_to_be_included = false
+    block_to_be_rescued = false
+    args.each do |arg|
+      case
+        when arg.kind_of?(Hash)
+          attributes.merge!(arg)
+        when arg.kind_of?(String)
+          pcdata += arg
+        when arg == :timestamp
+          attributes[:timestamp] = self.timestamp
+        when arg == :duration
+          duration_to_be_included = true
+        when arg == :rescue
+          block_to_be_rescued = true
+        else
+          pcdata = pcdata + arg.inspect
+      end
+    end
+    log_puts("BEGIN\t#{element_name}")
+    put_attributes(attributes)
+    unless pcdata.empty?
+      # Guard against using a terminator that's a substring of pcdata.
+      s = 'EOT'
+      terminator = s
+      while pcdata.match(terminator) do
+        terminator += s
+      end
+      log_puts("PCDATA\t<<#{terminator}")
+      log_puts(pcdata)
+      log_puts(terminator)
+    end
+    start_time = Time.new if duration_to_be_included
+    if block_given?
+      if block_to_be_rescued
+        begin
+          yield
+        rescue Exception => x
+          put_element('uncaught_exception') do
+            put_element('class', x.class)
+            put_element('message', x.message)
+            put_element('backtrace') do
+              cdata(filter_backtrace(x.backtrace))
+            end
+          end
+        end
+      else
+        yield
+      end
+    end
+    if start_time
+      end_time = Time.now
+      duration_f = end_time.to_f - start_time.to_f
+      duration_s = format('%.3f', duration_f)
+      put_attributes({:duration_seconds => duration_s})
+    end
+    log_puts("END\t#{element_name}")
+    nil
+  end
+
+  # Start a new section, within the current section.
+  # Sections may be nested.
+  def section(name, *args)
+    put_element('section', {:name => name}, *args) do
+      yield
+    end
+    nil
+  end
+
+  def comment(text, *args)
+    put_element('comment', text, *args)
+    nil
+  end
+
+  private
+
+  def initialize(options=Hash.new, im_ok_youre_not_ok = false)
+    unless im_ok_youre_not_ok
+      # Caller should call StructuredLog.open, not StructuredLog.new.
+      raise RuntimeError.new(NO_NEW_MSG)
+    end
+    self.file_path = options[:file_path]
+    self.root_name = options[:root_name]
+    self.xml_indentation = options[:xml_indentation]
+    self.backtrace_filter = options[:backtrace_filter] || /log|ruby/
+    self.file = File.open(self.file_path, 'w')
+    log_puts("REMARK\tThis text log is the precursor for an XML log.")
+    log_puts("REMARK\tIf the logged process completes, this text will be converted to XML.")
+    log_puts("BEGIN\t#{self.root_name}")
+    nil
+  end
+
+  def dispose
+
+    # Close the text log.
+    log_puts("END\t#{self.root_name}")
+    self.file.close
+
+    # Create the xml log.
+    document = REXML::Document.new
+    File.open(self.file_path, 'r') do |file|
+      element = document
+      stack = Array.new
+      data_a = nil
+      terminator = nil
+      file.each_line do |line|
+        line.chomp!
+        line_type, text = line.split("\t", 2)
+        case line_type
+          when 'REMARK'
+            next
+          when 'BEGIN'
+            element_name = text
+            element = element.add_element(element_name)
+            stack.push(element)
+          when 'END'
+            stack.pop
+            element = stack.last
+          when 'ATTRIBUTE'
+            attr_name, attr_value = text.split("\t", 2)
+            element.add_attribute(attr_name, attr_value)
+          when 'CDATA'
+            stack.push(:cdata)
+            data_a = Array.new
+            terminator = text.split('<<', 2).last
+          when 'PCDATA'
+            stack.push(:pcdata)
+            data_a = Array.new
+            terminator = text.split('<<', 2).last
+          when terminator
+            data_s = data_a.join("\n")
+            data_a = nil
+            terminator = nil
+            data_type = stack.last
+            case data_type
+              when :cdata
+                cdata = CData.new(data_s)
+                element.add(cdata)
+              when :pcdata
+                element.add_text(data_s)
+              else
+                # Don't want to raise an exception and spoil the run
+            end
+            stack.pop
+          else
+            data_a.push(line) if (terminator)
+        end
+      end
+      document << XMLDecl.default
+    end
+
+    File.open(self.file_path, 'w') do |file|
+      document.write(file, self.xml_indentation)
+    end
+    nil
+  end
+
+  def put_attributes(attributes)
+    attributes.each_pair do |name, value|
+      value = case
+                when value.is_a?(String)
+                  value
+                when value.is_a?(Symbol)
+                  value.to_s
+                else
+                  value.inspect
+              end
+      log_puts("ATTRIBUTE\t#{name}\t#{value}")
+    end
+    nil
+  end
+
+  def log_puts(text)
+    self.file.puts(text)
+    self.file.flush
+    nil
+  end
+
+  def cdata(text)
+    # Guard against using a terminator that's a substring of the cdata.
+    s = 'EOT'
+    terminator = s
+    while text.match(terminator) do
+      terminator += s
+    end
+    log_puts("CDATA\t<<#{terminator}")
+    log_puts(text)
+    log_puts(terminator)
+    nil
+  end
+
+  # Filters lines that are from ruby or log, to make the backtrace more readable.
+  def filter_backtrace(lines)
+    filtered = ['']
+    lines.each do |line|
+      unless line.match(self.backtrace_filter)
+        filtered.push(line)
+      end
+    end
+    filtered.join("\n")
+  end
+
+  # Return a timestamp string.
+  # The important property of this string
+  # is that it can be incorporated into a legal directory path
+  # (i.e., has no colons, etc.).
+  def self.timestamp
+    now = Time.now
+    ts = now.strftime('%Y-%m-%d-%a-%H.%M.%S')
+    usec_s = (now.usec / 1000).to_s
+    while usec_s.length < 3 do
+      usec_s = '0' + usec_s
+    end
+    # noinspection RubyUnusedLocalVariable
+    ts += ".#{usec_s}"
+  end
+
+end
---
+++ test/structured_log_test.rb
@@ -0,0 +1,145 @@
+require 'minitest/autorun'
+require 'set'
+
+require_relative '../lib/structured_log'
+
+class StructuredLogTest < MiniTest::Test
+
+  # Create a temporary logfile.
+  # Caller should provide a block to be executed using the log.
+  # Returns the file path to the closed log.
+  def create_temp_log
+    dir_path = Dir.mktmpdir
+    file_path = File.join(dir_path, 'log.xml')
+    StructuredLog.open({:file_path => file_path}) do |log|
+      yield log
+    end
+    file_path
+  end
+
+  # Helper class for checking logged output.
+  class Checker
+
+    attr_accessor \
+      :exceptions,
+      :file_path,
+      :root,
+      :test
+
+    # - +test+:  +MiniTest::Test+ object, to make assertions available.
+    # - +file_path+:  Path to log file.
+    def initialize(test, file_path)
+      # Needs the test object for accessing assertions.
+      self.test = test
+      self.file_path = file_path
+      # Clean up after.
+      ObjectSpace.define_finalizer(self, method(:finalize))
+      File.open(file_path, 'r') do |file|
+        self.root = REXML::Document.new(file).root
+      end
+      nil
+    end
+
+    # To clean up the temporary directory.
+    # - +object_id+:  Id of temp directory.
+    def finalize(object_id)
+      file_path = ObjectSpace._id2ref(object_id).file_path
+      File.delete(file_path)
+      Dir.delete(File.dirname(file_path))
+      nil
+    end
+
+    def assert_root_name(name)
+      test.assert_equal(name, self.root.name)
+    end
+
+    def assert_xml_indentation(indentation)
+      File.open(file_path) do |file|
+        lines = file.readlines
+        case indentation
+          when -1
+            # Should all be on one line; no whitespace.
+            test.assert_equal(1, lines.size)
+          when 0
+            # Should be multiple lines, but no indentation.
+            test.assert_operator(1, :<, lines.size)
+            test.refute_match(/^ /, lines[1])
+          when 2
+            # Should be multiple lines, with 2-space indentation.
+            test.assert_operator(1, :<, lines.size)
+            test.assert_match(/^  \S/, lines[1])
+          else
+            raise NotImplementedError(indentation)
+        end
+      end
+    end
+
+  end
+
+  def test_new
+    exception = assert_raises(RuntimeError) do
+      StructuredLog.new
+    end
+    assert_equal(StructuredLog::NO_NEW_MSG, exception.message)
+  end
+
+  def test_open_no_block
+    exception = assert_raises(RuntimeError) do
+      StructuredLog.open
+    end
+    assert_equal(StructuredLog::NO_BLOCK_GIVEN_MSG, exception.message)
+  end
+
+  def test_open_block
+    file_path = StructuredLog.open do |_|
+    end
+    Checker.new(self, file_path)
+  end
+
+  def test_open_default_file_path
+    file_path = StructuredLog.open do |log|
+      assert_equal('./log.xml', log.file_path)
+    end
+    Checker.new(self, file_path)
+  end
+
+  def test_open_file_path
+    file_path = StructuredLog.open(:file_path => 'foo.xml') do |log|
+      assert_equal('foo.xml', log.file_path)
+    end
+    Checker.new(self, file_path)
+  end
+
+  def test_open_root_name
+    root_name = 'foo'
+    file_path = StructuredLog.open(:root_name => root_name) do |_|
+    end
+    checker = Checker.new(self, file_path)
+    checker.assert_root_name(root_name)
+  end
+
+  def test_open_xml_indentation
+    [-1, 0, 2].each do |indentation|
+      file_path = StructuredLog.open(:xml_indentation => indentation) do |log|
+        log.section('Section') do
+        end
+      end
+      checker = Checker.new(self, file_path)
+      checker.assert_xml_indentation(indentation)
+    end
+
+  end
+
+  def test_section
+
+  end
+
+  def test_put_element
+
+  end
+
+  def test_comment
+
+  end
+
+end
